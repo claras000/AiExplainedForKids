6180a6bdf2717699fa8566c303b087da9b1f78a2
claras000 <clara.o.correa@web.de>
2024-05-17 12:05:02 +0100

    readme

--- gameWordScramble/js/words.js
+++ gameWordScramble/js/words.js
@@ -1,12 +1,15 @@
 let words = [
   {
     word: "Robot",
-    hint: "A machine that does tasks automatically, like a helper.",
+    hint: "I'm a smart friend in a machine, with circuits and wires I gleam. What am I? ",
   },
   {
-    word: "Code",
+    word: "Computer",
     hint: "Instructions that tell the robot what to do, like a secret language.",
   },
 
-  { word: "Friend", hint: "A computer buddy that learns and talks to you." },
+  {
+    word: "Friend",
+    hint: "I’m someone who shares toys and games, and always remembers your name. Who am I?",
+  },
 ];
---
+++ gameWordScrambleBadEnding/js/words.js
@@ -0,0 +1,12 @@
+let words = [
+  {
+    word: "Ant",
+    hint: "⁠I’m part of a colony, living in a hill, working with friends, never standing still. Who am I?",
+  },
+  {
+    word: "NLP",
+    hint: "I am a robot that learns to talk with you, What am I?",
+  },
+
+  { word: "dangerous", hint: "Inventing Articial Intelligence can be.." },
+];
